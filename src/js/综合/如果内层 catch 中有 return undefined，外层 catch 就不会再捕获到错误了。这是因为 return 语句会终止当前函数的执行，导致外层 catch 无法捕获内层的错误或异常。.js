f3d39f2d2bd2411b50f16cb79f85f18ef6044391
hua.dou <hua.dou@happyelements.com>
2025-02-18 11:09:21 +0800

    code

--- src/js/综合/如果内层 catch 中有 return undefined，外层 catch 就不会再捕获到错误了。这是因为 return 语句会终止当前函数的执行，导致外层 catch 无法捕获内层的错误或异常。.js
+++ src/js/综合/如果内层 catch 中有 return undefined，外层 catch 就不会再捕获到错误了。这是因为 return 语句会终止当前函数的执行，导致外层 catch 无法捕获内层的错误或异常。.js
@@ -29,3 +29,33 @@ console.log('Result:', result);
 
 // Inside inner catch: Inner error
 // Result: undefined
+
+
+/**
+ * 如果你希望内层 catch 返回 undefined 之后，仍然让外层的 catch 捕获异常，你需要确保 异常抛出 传递给外层。
+ *
+ * 示例：重新抛出错误
+ */
+
+function outerFunction2() {
+    try {
+        try {
+            // 内层代码，抛出一个异常
+            throw new Error('Inner error');
+        } catch (innerError) {
+            // 内层捕获异常并返回 undefined，但重新抛出异常
+            console.log('Inside inner catch:', innerError.message);
+            throw innerError;  // 重新抛出异常到外层 catch
+        }
+    } catch (outerError) {
+        // 外层捕获异常
+        console.log('Inside outer catch:', outerError.message);
+    }
+}
+
+let result2 = outerFunction2();
+console.log('Result2:', result2);
+
+// Inside inner catch: Inner error
+// Inside outer catch: Inner error
+// Result2: undefined
